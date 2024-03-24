32c06d002edd26002d26139f1975ca727969a341
Michael Osachoff <mwo678@uregina.ca>
2024-03-24 00:18:16 -0600

    Updated darts to have 501 to 0 with win and busts

--- Bang!/Systems/DartGame/Dart.gd
+++ Bang!/Systems/DartGame/Dart.gd
@@ -1,1 +1,13 @@
 class_name Dart extends RigidBody3D
+
+signal timeout_despawn
+
+func start_timer():
+	$Timer.start()
+
+func stop_timer():
+	$Timer.stop()
+
+func _on_timer_timeout():
+	timeout_despawn.emit()
+	self.queue_free()
